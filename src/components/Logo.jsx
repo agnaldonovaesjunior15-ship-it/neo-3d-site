053c5ed274b619b54c42d16dc7e5711629941c5c
agnaldonovaesjunior15-ship-it <agnaldonovaesjunior15@gmail.com>
2026-06-16 19:47:10 -0300

    Corrige extensao do logo para PNG

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import logoImg from '../assets/logo.gif'
+import logoImg from '../assets/logo.png'
 import './Logo.css'
 
 export default function Logo({ showTagline = false }) {
